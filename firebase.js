4d13220480ced423720fafa912205eb827579276
ruzagirizakadochristiano-del <ruzagirizakadochristiano@gmail.com>
2026-05-18 19:41:02 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+ const firebaseConfig = {
+        apiKey: "AIzaSyDZxBIpI6QdN1MjK3t1N_m4BVfEEoI_SGY",
+        authDomain: "rolling-watch-collection.firebaseapp.com",
+        projectId: "rolling-watch-collection",
+        storageBucket: "rolling-watch-collection.firebasestorage.app",
+        messagingSenderId: "780300275683",
+        appId: "1:780300275683:web:05a06771ac53c744e3dad6"
+    };
